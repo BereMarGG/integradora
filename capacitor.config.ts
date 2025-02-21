701522c3a1469d4c3629429950ce70eb453befa6
BereMarGG <mberenicegarciagtz@gmail.com>
2025-02-20 23:26:36 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Home4Students',
+  webDir: 'www'
+};
+
+export default config;
